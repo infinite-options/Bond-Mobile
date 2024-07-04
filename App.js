0f5df8d9724790ba269677e062081a428ab3d6b3
varsana4 <vilangog@ucsc.edu>
2024-07-04 14:52:09 -0700

    helloworld

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-import AppNavigator from './AppNavigator'; 
-
-
-const App = () => {
+export default function App() {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>HelloWorld</Text>
+      <StatusBar style="auto" />
+    </View>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
